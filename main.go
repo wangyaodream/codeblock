30adbe234bcca62aad0bc28f2af1c155223d813b
wangyaodream <wangyao.dream@hotmail.com>
2024-11-06 17:53:54 +0800

    testing for richtext with fyne

--- main.go
+++ main.go
@@ -1,47 +1,55 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"strconv"
-	"time"
+	"net/url"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/widget"
 )
 
-func updateTime(clock *widget.Label) {
-	formated := time.Now().Format("Time: 03:04:05")
-	clock.SetText(formated)
-}
+var richtext = _richtext{}
 
-func updateNum(clock *widget.Label) {
-	randomInt := rand.Intn(100) + 1
-	randomIntString := strconv.Itoa(randomInt)
-	clock.SetText(randomIntString)
+type _richtext struct{}
 
+func (me _richtext) ui(w fyne.Window) {
+	lblMsg := widget.NewLabel("")
+	lblMsg.Alignment = fyne.TextAlignCenter
+
+	u, _ := url.Parse("https://github.com/fyne-io/fyne")
+	richtext1 := widget.NewRichText(
+		&widget.TextSegment{Text: "Text\t", Style: widget.RichTextStyleInline},
+		&widget.HyperlinkSegment{Text: "Link", URL: u},
+		&widget.SeparatorSegment{},
+		&widget.ImageSegment{Source: storage.NewFileURI("dragon.jpeg"), Title: "dragon"},
+		&widget.ListSegment{Items: []widget.RichTextSegment{
+			&widget.TextSegment{Text: "Text"},
+			&widget.HyperlinkSegment{Text: "Link"},
+		}, Ordered: true},
+		&widget.ParagraphSegment{Texts: []widget.RichTextSegment{
+			&widget.TextSegment{Text: "Text"},
+			&widget.HyperlinkSegment{Text: "Link"},
+		}},
+	)
+	richtext2 := widget.NewRichTextFromMarkdown("# title1 \n- item*1* \n- item**2** \n## title 3")
+	richtext3 := widget.NewRichTextWithText("test1\ntest2")
+
+	btn1 := widget.NewButton("btn1", func() {
+	})
+
+	c := container.NewVBox(lblMsg, richtext1, richtext2,
+		richtext3, btn1)
+
+	w.SetContent(c)
+	w.Show()
 }
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("demo")
-	w.SetContent(widget.NewLabel("Hello world!"))
-	w.Show()
-
-	w2 := a.NewWindow("Larger")
-	w2.SetContent(widget.NewButton("open", func() {
-		w3 := a.NewWindow("new widnow")
-		w3.SetContent(widget.NewLabel("Third"))
-		w3.Show()
-
-	}))
-	w2.Resize(fyne.NewSize(100, 100))
-	w2.Show()
+	richtext.ui(w)
 
 	a.Run()
-}
-
-func foo() {
-	fmt.Println("Run in exited！")
 }
