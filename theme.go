e6a90de6b122593e8667cf3e04962b6d47fea4f4
wangyaodream <wangyao.dream@hotmail.com>
2024-11-15 17:53:41 +0800

    add font style controller

--- theme.go
+++ theme.go
@@ -21,6 +21,15 @@ func (t *fysionTheme) Color(name fyne.ThemeColorName, _ fyne.ThemeVariant) color
 	return t.Theme.Color(name, theme.VariantLight)
 }
 
+func (t *fysionTheme) Font(s fyne.TextStyle) fyne.Resource {
+    if s.Symbol || s.Monospace {
+        return t.Theme.Font(s)
+    }
+
+    if s.Bold {
+    }
+}
+
 func (t *fysionTheme) Size(name fyne.ThemeSizeName) float32 {
 	if name == theme.SizeNameText {
 		return 12
